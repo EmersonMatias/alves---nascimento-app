3c585bdc712684475599ed9bc7ab250fc3c6ebb9
Emerson <emersonsmatthias@gmail.com>
2024-03-23 09:22:10 -0300

    init

---
+++ src/style/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        black: '#1E1E1E',
+        white: '#FFFFFF',
+        gold: '#FFD066',
+    },
+    fontFamily: {
+        montserrat: '"Montserrat", sans-serif',
+        cinzel: '"Cinzel", serif'
+    }
+}
