f9bbc9467f53c539832e29fbd17845b568e65d11
Emerson <emersonsmatthias@gmail.com>
2024-04-08 12:42:37 -0300

    feat: adicionado o tag manager

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,27 @@
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect } from "react";
 import Style from "./style";
 import Whatsapp from "../../assets/svg/Whatsapp.svg"
 import { sendToWpp } from "../../utils/send-to-social";
+import TagManager from "react-gtm-module"
 
 type ButtonProps = ComponentProps<'button'>
 
+
 export default function Button({ children }: ButtonProps) {
+
+    useEffect(() => {
+        const tagManagerArgs = {
+            gtmId: "GTM-TPGNWXR5"
+        }
+
+        TagManager.initialize(tagManagerArgs)
+    }, [])
+
+
     return (
-        <Style.Container onClick={sendToWpp}>
+        <Style.Container onClick={sendToWpp} id="buttonAds">
             {children}
-            <img src={Whatsapp} alt="Icone do Whatsapp" />
+            <img src={Whatsapp} alt="Icone do" />
         </Style.Container>
     )
 }
